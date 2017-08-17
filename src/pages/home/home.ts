8863b106d3982d28150b70bcdac8d2c250a0145f
Manish Bisht <manish.bisht490@gmail.com>
2017-08-17 16:14:52 +0530

    home display

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import Firebase from 'firebase';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  places: Array<string>;
 
+  constructor(public navCtrl: NavController) {
+    var database = Firebase.database();
+
+    var current = this;
+
+    this.places = [];
+
+    database.ref('/places/').on('value', function (snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        current.places.push(childSnapshot.val());
+        return false;
+      });
+    });
   }
 
 }
