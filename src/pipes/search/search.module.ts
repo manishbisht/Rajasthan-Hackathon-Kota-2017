760e988e948100c04e7a781e2d178695770e7f84
Manish Bisht <manish.bisht490@gmail.com>
2017-08-18 04:50:11 +0530

    addeed

---
+++ src/pipes/search/search.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { SearchPipe } from './search';
+
+@NgModule({
+    declarations: [ SearchPipe],
+    exports: [SearchPipe],
+})
+export class SearchPipeModule {
+}
